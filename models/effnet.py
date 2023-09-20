ddf41cae27774d8fda63cc57a8ce1a79f5af5063
Naman <70206828+naman2398@users.noreply.github.com>
2023-09-20 13:16:34 +0530

    Update effnet.py

--- models/effnet.py
+++ models/effnet.py
@@ -1,5 +1,33 @@
+from keras.applications import DenseNet201, ResNet152V2, InceptionV3, InceptionResNetV2
 from efficientnet.tfkeras import EfficientNetB7 as effnetb7
 
-base_model = effnetb7(include_top=False,
-                     weights = None,
-                     input_shape=(224,224,3))
+
+# Initialize DenseNet201 base model
+dense_net_model = DenseNet201(include_top=False,
+                              weights=None,
+                              input_shape=(224, 224, 3))
+
+
+# Initialize ResNet152V2 base model
+res_net_model = ResNet152V2(include_top=False,
+                            weights=None,
+                            input_shape=(224, 224, 3))
+
+
+# Initialize EfficientNetB7 base model
+eff_net_model = effnetb7(include_top=False,
+                         weights=None,
+                         input_shape=(224, 224, 3))
+
+
+# Initialize InceptionV3 base model
+inception_v3_model = InceptionV3(include_top=False,
+                                 weights=None,
+                                 input_shape=(224, 224, 3))
+
+
+# Initialize InceptionResNetV2 base model
+inception_resnet_v2_model = InceptionResNetV2(include_top=False,
+                                               weights=None,
+                                               input_shape=(224, 224, 3))
+
